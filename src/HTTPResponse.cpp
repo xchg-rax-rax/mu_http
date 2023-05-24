344444220ee4eb5680fa768e5dd4523137f08dd9
Lain <lain@trioptimum.com>
2023-05-24 13:43:29 +0100

    Added server and date headers

--- src/HTTPResponse.cpp
+++ src/HTTPResponse.cpp
@@ -1,45 +1,53 @@
 #include "HTTPResponse.h"
 
+#include <iomanip>
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <cstdint>
 #include <iterator>
+#include <sstream>
 #include <string>
 #include <filesystem>
 
 
 // These are silly, implement a function to prepare these as need (maybe even at compile time?)
-static constexpr auto bad_response_400_body = "<html>\
-<head><title>400 Bad Request</title></head>\
-<body bgcolor=\"white\">\
-<center><h1>400 Bad Request</h1></center>\
-</body>\
-</html>";
 
-static constexpr auto not_found_404_body = "<html>\
-<head><title>404 Not Found</title></head>\
-<body bgcolor=\"white\">\
-<center><h1>404 Not Found</h1></center>\
-</body>\
-</html>";
+/*
+static constexpr std::string generate_error_body(const std::string message) {
+    return "<html>\n<head><title>"s + message + "</title></head>\n<body bgcolor=\"white\">\n<center><h1>"s + message + "</h1></center>\n</body>\n</html>"s;
+}
+*/
 
-static constexpr auto internal_server_error_500_body= "<html>\
-<head><title>500 Internal Server Error</title></head>\
-<body bgcolor=\"white\">\
-<center><h1>500 Internal Server Error</h1></center>\
-</body>\
-</html>";
+static std::string generate_error_body(const std::string message) {
+    const std::string start = "<html>\n<head><title>";
+    const std::string middle = "</title></head>\n<body bgcolor=\"white\">\n<center><h1>";
+    const std::string end = "</h1></center>\n</body>\n</html>";
+    return start + message + middle + message + end;
+}
 
-static constexpr auto http_version_not_supported_505_body = "<html>\
-<head><title>505 HTTP Version not supported</title></head>\
-<body bgcolor=\"white\">\
-<center><h1>505 HTTP Version not supported</h1></center>\
-</body>\
-</html>";
+static const auto bad_response_400_body = generate_error_body("400 Bad Request");
 
+static const auto not_found_404_body = generate_error_body("404 Not Found");
+
+static const auto internal_server_error_500_body = generate_error_body("500 Internal Server Error");
+
+static const auto http_version_not_supported_505_body = generate_error_body("505 HTTP Version not supported");
+
+std::string HTTPResponse::generate_timestamp() {
+    auto now = std::chrono::system_clock::now();
+    auto now_as_time_t = std::chrono::system_clock::to_time_t(now);
+    std::tm *local = std::localtime(&now_as_time_t);
+    std::stringstream timestamp_ss;
+    timestamp_ss << std::put_time(local, "%a, %d %b %Y %T %Z");
+    return timestamp_ss.str();
+}
 
 void HTTPResponse::generate_response() {
+    // During optimization should probably be made to be static
     std::map<std::string, std::string> headers{};
+    headers["Server"] = "Mu HTTP Server/0.1";
+    headers["Date"] = generate_timestamp();
     if (!_request.valid()) {
         // Set fields for 400 response
         compose_response(StatusCode::bad_request, headers, bad_response_400_body);
@@ -53,7 +61,7 @@ void HTTPResponse::generate_response() {
         return;
     }
     // Should probably do this using std::filesystem::paths
-    // Also there's DEFINITELY directory traversal vulns here
+    // std::filesystem seems to be quite good at catching path traversal bugs but best to be safe
     const static std::string pwd = std::filesystem::current_path();
     std::string target_file_path =  pwd + std::string(_request.uri());
     if (target_file_path.ends_with("/")) {
--- src/HTTPResponse.h
+++ src/HTTPResponse.h
@@ -39,4 +39,5 @@ class HTTPResponse {
         void generate_response();
         void compose_response(StatusCode status_code, std::map<std::string, std::string> headers, std::string_view message_body);
         bool read_target_file(const std::string& target_file_path, std::string& target_file);
+        std::string generate_timestamp();
 };
